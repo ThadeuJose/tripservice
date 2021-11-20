bdb744c6eeddb4176bb9242cd071f4c44450be61
Thadeu José <thadeujs@gmail.com>
2021-11-19 23:30:06 -0300

    Add test shouldReturnEmptyListIfUserHasNoFriend

--- src/test/java/com/legacytest/TripServiceTest.java
+++ src/test/java/com/legacytest/TripServiceTest.java
@@ -1,6 +1,10 @@
 package com.legacytest;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.empty;
+import static org.hamcrest.Matchers.is;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+
 import com.legacytest.exception.UserNotLoggedInException;
 import com.legacytest.trip.TripService;
 import com.legacytest.user.User;
@@ -9,19 +13,29 @@ import org.junit.jupiter.api.Test;
 
 public class TripServiceTest {
 
-    User user;
+    User loggedUser;
 
     @Test
     public void shouldRaiseErrorIfUserNotLogged() {
         TripService tripService = new TestTripService();
-        user = null;
+        loggedUser = null;
         assertThrows(UserNotLoggedInException.class, () -> tripService.getTripsByUser(null));
     }
+
+    @Test
+    public void shouldReturnEmptyListIfUserHasNoFriend() {
+        TripService tripService = new TestTripService();
+        loggedUser = new User();
+        User bob = new User();
+        assertThat(tripService.getTripsByUser(bob), is(empty()));
+    }
+
+    // TODO: Test with User friend empty
 
     class TestTripService extends TripService {
         @Override
         protected User getLoggedUser() {
-            return user;
+            return loggedUser;
         }
     }
 
