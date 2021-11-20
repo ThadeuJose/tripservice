7de62a7004d503e7f350e777facbe77a31ff444e
Thadeu José <thadeujs@gmail.com>
2021-11-19 22:24:41 -0300

    Add test shouldRaiseErrorIfUserNotLogged

--- src/test/java/com/legacytest/TripServiceTest.java
+++ src/test/java/com/legacytest/TripServiceTest.java
@@ -1,13 +1,28 @@
 package com.legacytest;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import com.legacytest.exception.UserNotLoggedInException;
+import com.legacytest.trip.TripService;
+import com.legacytest.user.User;
 
 import org.junit.jupiter.api.Test;
 
 public class TripServiceTest {
 
+    User user;
+
     @Test
-    public void shouldAnswerWithTrue() {
-        assertTrue(true);
+    public void shouldRaiseErrorIfUserNotLogged() {
+        TripService tripService = new TestTripService();
+        user = null;
+        assertThrows(UserNotLoggedInException.class, () -> tripService.getTripsByUser(null));
     }
+
+    class TestTripService extends TripService {
+        @Override
+        protected User getLoggedUser() {
+            return user;
+        }
+    }
+
 }
